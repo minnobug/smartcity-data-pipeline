fe583903b90dd944aa70cff93480c03cdda6cf58
minnobug <levanminh2129@gmail.com>
2026-01-10 15:19:14 +0700

    feat: add GPS, traffic, weather information generator

--- jobs/main.py
+++ jobs/main.py
@@ -21,6 +21,7 @@ TRAFFIC_TOPIC = os.getenv('TRAFFIC_TOPIC', 'traffic_data')
 WEATHER_TOPIC = os.getenv('WEATHER_TOPIC', 'weather_data')
 EMERGENCY_TOPIC = os.getenv('EMERGENCY_TOPIC', 'emergency_data')
 
+random.seed(42)
 start_time = datetime.datetime.now()
 start_location = HO_CHI_MINH_COORDINATES.copy()
 
@@ -30,9 +31,43 @@ def get_next_time():
     start_time += datetime.timedelta(seconds=random.randint(30, 60)) # update frequency
     return start_time
 
+
+def generate_gps_data(device_id, timestamp, vehicle_type='private'):
+	return {
+		'id': uuid.uuid4(),
+		'deviceId': device_id,
+		'timestamp': timestamp,
+		'speed': random. uniform( 0, 40), # km/h
+		'direction': 'North-East',
+		'vehicleType': vehicle_type
+	}
+
+def generate_traffic_camera_data(device_id, timestamp, location, camera_id) :
+	return {
+		'id': uuid.uuid4(),
+		'deviceId': device_id,
+		'cameraId': camera_id,
+		'location': location,
+		'timestamp': timestamp,
+		'snapshot': 'Base64EncodedString'
+	}
+
+def generate_weather_data(device_id, timestamp, location):
+	return {
+		'id': uuid.uuid4(),
+		'deviceId': device_id,
+		'location': location,
+		'timestamp': timestamp,
+		'temperature' : random.uniform(-5,26),
+		'weatherCondition': random.choice(['Sunny', 'Cloudy', 'Rainy', 'Snow'])
+		'precipitation': random.uniform(0,25),
+		'windSpeed': random.uniform(0,100),
+		'humidity': random.uniform(0,100), # percentage
+		'airQualityIndex': random.uniform(0,500), # AQL value goes there
+	}
 def simulate_vehicle_movement():
 	global start_location
-	#move towards Vung_Tau
+	# move towards Vung_Tau
 	start_location['latitude'] += LATITUDE_INCREMENT
 	start_location['longitude'] += LONGITUDE_INCREMENT
 
@@ -58,9 +93,12 @@ def generate_vehicle_data(device_id):
         }
 
 def simulate_journey(producer, device_id):
-	while True:
-	    vehicle_data = generate_vehicle_data(device_id)
-
+    while True:
+		vehicle_data = generate_vehicle_data(device_id)
+		gps_data = generate_gps_data(device_id, vehicle_data['timestamp'])
+		traffic_camera_data = generate_traffic_camera_data(device_id, vehicle_data['timestamp'], 'Nikon-Cam123')
+		weather_data = generate_weather_data(device_id, vehicle_data['timestamp'], vehicle_data['location'])
+		break
 
 if __name__ == "__main__":
 
